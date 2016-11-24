5f8093979d0a4827fe3758f16989cdfebe843cb1
Henry <correia.eh@gmail.com>
2016-11-23 23:21:15 -0500

    Issue #7 - refactor the implement function to be part of the interface api. Update the tests to reflect the change. Rename src to lib

--- tests/interfaceJS.test.js
+++ tests/interfaceJS.test.js
@@ -1,9 +1,9 @@
-const { invalidPropError, missingPropError, unknownTypesError, wrongTypeError } = require('./src/errors/_errors.js');
-const Interface = require('./src/index.js');
+const { invalidPropError, missingPropError, unknownTypesError, wrongTypeError } = require('../lib/errors/_errors.js');
+const Interface = require('../lib/interfaceJS.js');
 
 describe('Creating a new interface', () => {
     it('creates an interface with valid allowed types', () => {
-        let {id, props, implement} = Interface.create('person', {
+        let {id, props} = Interface.create('person', {
             name: 'string',
             age: 'number',
             greet: 'function',
@@ -15,7 +15,6 @@ describe('Creating a new interface', () => {
             age: 'number',
             greet: 'function',
         });
-        expect(typeof implement).toBe('function');
     });
 
     it('cannot create interface with unknown types', () => {
@@ -41,7 +40,7 @@ describe('Implement an interface by creating an object', () => {
 
     it('should correctly create an object implementing the interface if all types and properties are passed', () =>{
         expect(() => (
-            personInterface.implement({
+            Interface.implement(personInterface, {
                 nickname: 'Henry',
                 age: 22,
                 greet() { return `hi my name is ${this.name}`; },
@@ -50,7 +49,7 @@ describe('Implement an interface by creating an object', () => {
     });
 
     it('should add defined functions to the object\'s prototype', () => {
-        let brittany = personInterface.implement({
+        let brittany = Interface.implement(personInterface, {
             nickname: 'Brinny',
             age: 25,
             greet() { return `Howdy all! I'm ${this.name}`; },
@@ -61,7 +60,7 @@ describe('Implement an interface by creating an object', () => {
 
     it('should throw an error if the wrong type is passed as the implementing object', () => {
         expect(() => (
-            personInterface.implement({
+            Interface.implement(personInterface, {
                 nickname: 'Brinny',
                 age: '26',
                 greet() { return `Hi everyone, I'm ${this.name} and I'm ${this.age}`; },
@@ -71,7 +70,7 @@ describe('Implement an interface by creating an object', () => {
 
     it('should throw an error if an interface\'s prop is not defined when implementing', () => {
         expect(() => (
-            personInterface.implement({
+            Interface.implement(personInterface, {
                 nickname: 'Brinny',
                 greet() { return `Hi everyone, I'm ${this.name} and I'm ${this.age}`; },
             })        
@@ -80,7 +79,7 @@ describe('Implement an interface by creating an object', () => {
 
     it('should throw a custom error if trying to add a property that is not defined in the interface', () => {
         expect(() => (
-            personInterface.implement({
+            Interface.implement(personInterface, {
                 nickname: 'Brinny',
                 age: 25,
                 id: 'S2324356',
